e8f0f66ac682bf0b3a8ad657a5a57629bad4a453
Reploid20XX <jvargas7130@gmail.com>
2023-09-28 15:42:04 -0700

    Mobile and Contact
    Created mobile version using chrome responsive mobile inspector by fitting all sections mainly by using flex column and max-width.
    
    Implemented a  mobile menu by using mobil menu from my pup website and tweaking it a bit to fit my portfolio website.
    
    issues I ran into:
    Could not get menu to appear over landing page. Attempted to use position absolute on landing page and relative for mobile menu. As a result, the below div was overflowing into landing page. The solution was to make mobile menu position as fixed
    
    Implemented contact javascript with email server subscription emailjs.com.

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var skillsTab = document.querySelector('.skills-tab');
 var experienceTab = document.querySelector(".experience-tab");
 var educationTab = document.querySelector(".education-tab");
 
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+var tabLinks = document.getElementsByClassName("tab-links");
+
+
+var mobileLinks = document.getElementById('mobile-nav-links');
 
 
 
@@ -15,7 +17,7 @@ var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(subject){
   console.log( "what: " + subject);
 
-    for(tablist of tablinks){
+    for(tablist of tabLinks){
         tablist.classList.remove("active-link");
        
     }
@@ -51,7 +53,73 @@ function opentab(subject){
 
 }
 
+//Used in mobile version. The menu begins in closed 
+//"display-none" status. The "display-block" status is to display menu.
+function nav(){
+    if(mobileLinks.style.display === "none"){
+        mobileLinks.style.display = "block";
+        return;
+    }else {
+        mobileLinks.style.display = "none";
+        return;
+    }
+}
 
+//Contacts
+function sendMail() {
+    console.log("sent email");
+  
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+  
+    };
+    const serviceID = "service_yof9mqm";
+    const templateID = "template_6wb7jct";
+  console.log("mail");
+    emailjs.send(serviceID, templateID, params)
+      .then(
+        res => {
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log("res " + res);
+          alert("your message sent successfully")
+        })
+      .catch((err) => console.log(err));
+  };
 
+  /*Checks to validate name, email and message on contact us form  */
+function validation() {
+    console.log("validation1");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+    error_message.style.padding = "10px";
+    if (name.length < 3) {
+      console.log("validation2");
+      text = "please enter valid name";
+      error_message.innerHTML = text;
+      return false; //breaks out and returns false
+    }
+    if (email.indexOf("@") == -1 || email.length < 6 || email.indexOf(".") == -1) {
+      console.log("validation3");
+      text = "please enter valid email";
+      error_message.innerHTML = text;
+      return false; //breaks out and returns false
+    }
+    if (message.length <= 10) {
+      text = "Please enter more than 10 characters";
+      error_message.innerHTML = text;
+      return false;//breaks out and returns false
+    }
+    console.log("Send mail");
+    sendMail();//sends message to email
+    error_message.remove();//renoves div with error message 
+  };
+  
 
 
